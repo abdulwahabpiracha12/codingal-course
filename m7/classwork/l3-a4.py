db4bd563cff955990b62734c1bd9a7484f6d8b65
abdulwahabpiracha12 <abdulwahabpiracha15@gmail.com>
2025-07-31 13:51:54 +0500

    initial commit

--- m7/classwork/l3-a4.py
+++ m7/classwork/l3-a4.py
@@ -1,4 +1,8 @@
-import datetime, calendar
-current_time = datetime.datetime.today()
-print("Time now at greenwich meridian is :",current_time)
-print("/n", calendar.calendar(3065))
+import datetime
+import calendar
+
+# Get current time and manually adjust for Pakistan (UTC+5)
+current_time = datetime.datetime.utcnow() + datetime.timedelta(hours=5)
+
+print("Time now in Pakistan is:", current_time)
+print("\n", calendar.calendar(2025))
