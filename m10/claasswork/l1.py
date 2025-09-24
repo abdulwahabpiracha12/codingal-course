f9296e42ab88809100ccf3599dd74a11edf6d47a
abdulwahabpiracha12 <abdulwahabpiracha15@gmail.com>
2025-09-24 16:37:53 +0500

    initial commit

--- m10/claasswork/l1.py
+++ m10/claasswork/l1.py
@@ -1,17 +1,17 @@
 import time
 
-
+#Most efficent: Irrespective of the value of n the number of iterations is just 1.
 def fun1(n):
   return n*(n+1)/2
 
-
+#As the value of n increases so does the number of iterations increases.
 def fun2(n):
   sum = 0
   for i in range(1, n+1) :
       sum += i
   return sum
 
-
+#Least efficent: The number of iterations increase at a faster pace than the value of n itself
 def fun3(n):
     sum = 0
     for i in range(1, n+1):
